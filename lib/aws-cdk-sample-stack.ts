869c26869e4ad3812f847724bdc9927a11ee9603
GakutoOkawara <ookawara0817@gmail.com>
2023-05-21 20:27:22 +0900

    remove comment out and add hello-world.ts

--- lib/aws-cdk-sample-stack.ts
+++ lib/aws-cdk-sample-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const func = new NodejsFunction(this, "Function", {
+      entry: "lib/hello-world.ts",
+      runtime: Runtime.NODEJS_18_X
+    })
   }
 }
